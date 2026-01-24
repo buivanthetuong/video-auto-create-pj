ab91b2fb366c6ef2855b201a96272e997b3964bb
Tường Chăm Chỉ <buivanthetuong123@gmail.com>
2026-01-24 16:34:52 +0700

    sua loi trung id

--- src/rootComponents/CSK001/data.js
+++ src/rootComponents/CSK001/data.js
@@ -11,28 +11,52 @@ import {
 
 const CMD_Fetch = CMD;
 
-//nhom video
-let Data_middle_001 = [];
-for (let i = 0; i < DataFront.length; i += 2) {
-  Data_middle_001.push(DataFront.slice(i, i + 2));
-}
-console.log("Data_middle_001", JSON.stringify(Data_middle_001));
-
 //add layout
 let videoData01 = [];
 
-Data_middle_001.forEach((e, i) => {
-  videoData01.push(group001_type02(e));
+DataFront.forEach((videoData) => {
+  let video = [];
+  let group = [];
+  let flag = videoData[0].group;
+  videoData.forEach((obj) => {
+    if (obj.group === flag) {
+      group.push(obj);
+    } else {
+      group = group001_type02(group);
+      for (let i = 0; i < group.length; i++) {
+        video.push(group[i]);
+      }
+      group = [obj];
+      flag = obj.group;
+    }
+  });
+  group = group001_type02(group);
+  for (let i = 0; i < group.length; i++) {
+    video.push(group[i]);
+  }
+
+  videoData01.push(video);
 });
+
 console.log(JSON.stringify(keepOnlyActionsCodeTimeFixedStt(videoData01)));
 export { videoData01 };
 
 function group001_type02(arr) {
+  const uid = `${arr[0].group}_${arr[0].code}`;
+
+  const BG001 = `BG001_${uid}`;
+  const div1 = `div1_${uid}`;
+  const divA = `divA_${uid}`;
+  const divB = `divB_${uid}`;
+  const divC = `divC_${uid}`;
+  const divD = `divD_${uid}`;
+  const textId = `text_${uid}`;
+
   const obj_001 = {
     actions: [
       {
         cmd: "divAction",
-        id: "BG001",
+        id: BG001,
         ToEndFrame: true,
         styleCss: {
           position: "absolute",
@@ -44,7 +68,7 @@ function group001_type02(arr) {
       },
       {
         cmd: CMD_Fetch.imageViewActionToID,
-        toID: "BG001",
+        toID: BG001,
         ToEndFrame: true,
         img: arr[0].backgroundIMG,
         styleCss: {
@@ -58,7 +82,7 @@ function group001_type02(arr) {
       },
       {
         cmd: CMD_Fetch.divAction,
-        id: "div1",
+        id: div1,
         group: arr[0].group,
         styleCss: {
           position: "absolute",
@@ -69,8 +93,8 @@ function group001_type02(arr) {
       },
       {
         cmd: CMD_Fetch.divAction,
-        id: "divA",
-        toID: "div1",
+        id: divA,
+        toID: div1,
         group: arr[0].group,
         styleCss: {
           display: "flex",
@@ -83,8 +107,8 @@ function group001_type02(arr) {
       {
         cmd: CMD_Fetch.typingText,
         text: arr[0].text,
-        id: "text",
-        toID: "divA",
+        id: textId,
+        toID: divA,
         group: arr[0].group,
         styleCss: {
           fontSize: "96px",
@@ -103,8 +127,8 @@ function group001_type02(arr) {
       },
       {
         cmd: CMD_Fetch.divAction,
-        id: "divB",
-        toID: "div1",
+        id: divB,
+        toID: div1,
         group: arr[0].group,
         styleCss: {
           display: "flex", // ⭐
@@ -116,8 +140,8 @@ function group001_type02(arr) {
       },
       {
         cmd: CMD_Fetch.divAction,
-        id: "divC",
-        toID: "divB",
+        id: divC,
+        toID: divB,
         group: arr[0].group,
         styleCss: {
           flexBasis: "100%",
@@ -130,7 +154,7 @@ function group001_type02(arr) {
       {
         cmd: CMD_Fetch.imageViewActionToID,
         img: arr[0].img,
-        toID: "divC",
+        toID: divC,
         group: arr[0].group,
         styleCss: {
           width: "600px",
@@ -142,8 +166,8 @@ function group001_type02(arr) {
       },
       {
         cmd: CMD_Fetch.divAction,
-        id: "divD",
-        toID: "divB",
+        id: divD,
+        toID: divB,
         group: arr[0].group,
         styleCss: {
           height: "auto",
@@ -157,7 +181,7 @@ function group001_type02(arr) {
       {
         cmd: CMD_Fetch.imageViewActionToID,
         img: arr[1].img,
-        toID: "divD",
+        toID: divD,
         group: arr[0].group,
         styleCss: {
           width: "600px",
@@ -175,7 +199,7 @@ function group001_type02(arr) {
     actions: [
       {
         cmd: CMD_Fetch.actionCssId,
-        toID: "text",
+        toID: textId,
         mode: "replace",
         css: {
           display: "none",
@@ -184,7 +208,7 @@ function group001_type02(arr) {
       {
         cmd: CMD_Fetch.typingText,
         text: arr[1].text,
-        toID: "divA",
+        toID: divA,
         delay: 15,
         group: arr[0].group,
         styleCss: {
@@ -198,7 +222,7 @@ function group001_type02(arr) {
 
       {
         cmd: CMD_Fetch.actionCssId,
-        toID: "divC",
+        toID: divC,
         mode: "add",
         css: {
           animation: "collapseWidth 1s ease-in-out forwards",
@@ -214,6 +238,194 @@ function group001_type02(arr) {
   finalSet.push(obj_002);
   return finalSet;
 }
+
+// function group001_type02(arr) {
+//   const obj_001 = {
+//     actions: [
+//       {
+//         cmd: "divAction",
+//         id: "BG001",
+//         ToEndFrame: true,
+//         styleCss: {
+//           position: "absolute",
+//           top: 0,
+//           bottom: 0,
+//           left: 0,
+//           right: 0,
+//         },
+//       },
+//       {
+//         cmd: CMD_Fetch.imageViewActionToID,
+//         toID: "BG001",
+//         ToEndFrame: true,
+//         img: arr[0].backgroundIMG,
+//         styleCss: {
+//           position: "absolute",
+//           top: 0,
+//           left: 0,
+//           width: "100%",
+//           height: "100%",
+//           objectFit: "cover",
+//         },
+//       },
+//       {
+//         cmd: CMD_Fetch.divAction,
+//         id: "div1",
+//         group: arr[0].group,
+//         styleCss: {
+//           position: "absolute",
+//           height: "100%",
+//           width: "100%",
+//           padding: "50px",
+//         },
+//       },
+//       {
+//         cmd: CMD_Fetch.divAction,
+//         id: "divA",
+//         toID: "div1",
+//         group: arr[0].group,
+//         styleCss: {
+//           display: "flex",
+//           alignItems: "center",
+//           justifyContent: "center",
+//           height: "40%",
+//           width: "100%",
+//         },
+//       },
+//       {
+//         cmd: CMD_Fetch.typingText,
+//         text: arr[0].text,
+//         id: "text",
+//         toID: "divA",
+//         group: arr[0].group,
+//         styleCss: {
+//           fontSize: "96px",
+//           fontWeight: 800,
+//           letterSpacing: "2px",
+//           lineHeight: "1.1",
+//           textTransform: "uppercase",
+//           WebkitTextStroke: "3px #fff",
+//           textShadow: "0 0 20px rgba(255,255,255,0.4)",
+//           animation: "bounceIn 0.5s ease-out forwards",
+//         },
+//       },
+//       {
+//         cmd: CMD_Fetch.soundPlayerAction,
+//         soundSource: "SOUNDCHUNG_tiktokTypingSoundCapcut",
+//       },
+//       {
+//         cmd: CMD_Fetch.divAction,
+//         id: "divB",
+//         toID: "div1",
+//         group: arr[0].group,
+//         styleCss: {
+//           display: "flex", // ⭐
+//           flexDirection: "row", // ⭐ mặc định
+//           gap: "20px", // optional
+//           height: "40%",
+//           padding: "50px",
+//         },
+//       },
+//       {
+//         cmd: CMD_Fetch.divAction,
+//         id: "divC",
+//         toID: "divB",
+//         group: arr[0].group,
+//         styleCss: {
+//           flexBasis: "100%",
+//           flexShrink: 0,
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         },
+//       },
+//       {
+//         cmd: CMD_Fetch.imageViewActionToID,
+//         img: arr[0].img,
+//         toID: "divC",
+//         group: arr[0].group,
+//         styleCss: {
+//           width: "600px",
+//           height: "600px",
+//           maxWidth: "100%",
+//           maxHeight: "100%",
+//           borderRadius: "20%",
+//         },
+//       },
+//       {
+//         cmd: CMD_Fetch.divAction,
+//         id: "divD",
+//         toID: "divB",
+//         group: arr[0].group,
+//         styleCss: {
+//           height: "auto",
+//           flexGrow: 1,
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         },
+//       },
+
+//       {
+//         cmd: CMD_Fetch.imageViewActionToID,
+//         img: arr[1].img,
+//         toID: "divD",
+//         group: arr[0].group,
+//         styleCss: {
+//           width: "600px",
+//           height: "600px",
+//           maxWidth: "100%",
+//           maxHeight: "100%",
+//         },
+//       },
+//     ],
+//     code: arr[0].code,
+//     timeFixed: 2,
+//     stt: 0,
+//   };
+//   const obj_002 = {
+//     actions: [
+//       {
+//         cmd: CMD_Fetch.actionCssId,
+//         toID: "text",
+//         mode: "replace",
+//         css: {
+//           display: "none",
+//         },
+//       },
+//       {
+//         cmd: CMD_Fetch.typingText,
+//         text: arr[1].text,
+//         toID: "divA",
+//         delay: 15,
+//         group: arr[0].group,
+//         styleCss: {
+//           textAlign: "center",
+//           fontSize: "60px",
+//           fontWeight: 700,
+//           WebkitTextStroke: "2px yellow",
+//           color: "#fff",
+//         },
+//       },
+
+//       {
+//         cmd: CMD_Fetch.actionCssId,
+//         toID: "divC",
+//         mode: "add",
+//         css: {
+//           animation: "collapseWidth 1s ease-in-out forwards",
+//         },
+//       },
+//     ],
+//     code: arr[1].code,
+//     timeFixed: 5,
+//     stt: 1,
+//   };
+//   let finalSet = [];
+//   finalSet.push(obj_001);
+//   finalSet.push(obj_002);
+//   return finalSet;
+// }
 
 //type2
 // function group001_type02(arr) {
